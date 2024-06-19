abc168bdc2fe6e092a8782fedcc5582ec1cb7c86
Mathieu <mathieu.vu@inria.fr>
2024-06-19 14:23:18 +0000

    Nouvelle structure d unrolling

--- TITAN_Unrolled/main.py
+++ TITAN_Unrolled/main.py
@@ -1,18 +1,64 @@
 import torch
 import torch.optim as optim
-from model import TitanIVAGNet
+from model import myModel, ISI_loss
 from data import *
 from tools import *
+from functions import *
 from torch.utils.data import DataLoader
+from torch import nn
+import os
+import sys
 
-# Define the number of epochs and other parameters
-num_epochs = 100
-T = 1000
+
+
+
+
+
+
+
+
+
+""" 
+for name, param in model.alpha_network.named_parameters():
+    print(name, param.requires_grad)
+
+def print_gradients(module, grad_input, grad_output):
+    print(f'Gradients for {module}: {grad_output}')
+
+
+
+
+ """
+
+
+
+
+
+# After training, you can access the trained model parameters, including the optimized alpha values, using model.parameters().
+
+
+
+
+
+
+
+
+
+
+## Model parameters
+
+gamma_c = 1
+gamma_w = 0.99
+eps = 1e-12
+nu = 0.5
+zeta = 0.1 
+
+
+# Hyperparameters
+
+T = 100
 K = 2
 N = 3
-
-input_dim = N * N * K + K * K * N 
-num_layers = 20
 
 lambda_1 = 0.04
 lambda_2 = 0.25
@@ -25,61 +71,186 @@ metaparameters_multiparam = get_metaparameters(rhos,lambdas)
 metaparameters_titles_multiparam = ['Case A','Case B','Case C','Case D']
 
 
-
-
-gamma_c = 1
-gamma_w = 0.99
-eps = 1e-12
-nu = 0.5
-size = 100
-zeta = 0.1 
-
-
-# créer le dataset
-dataset = MonDataset(T, K, N, metaparameters_multiparam, size = size)
-
-# créer le dataloader
-dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+input_dim = N * N * K + K * K * N 
+N_updates_W = 10
+N_updates_C = 1
+dataset_size = 10
+learning_rate = 1e-1
+num_epochs = 10
+batch_size = 10
+num_layers = 5
 
 
 
-# Now, when creating the optimizer, collect parameters from all layers
-model = TitanIVAGNet(input_dim, num_layers=num_layers, gamma_c=gamma_c, gamma_w=gamma_w, eps=eps, nu=nu, zeta=zeta).cuda()
+class U_TITAN(nn.Module):
+    """
+    Includes the main training and testing methods of U_TITAN.
 
-# Collect parameters from all alpha networks
-alpha_net_params = []
-for layer in model.layers:
-    alpha_net_params.extend(layer.alpha_net.parameters())
+    """
+
+    def __init__(self, mode='greedy', T=T, K=K, N=N, metaparameters_multiparam=metaparameters_multiparam, size = dataset_size, input_dim = input_dim,
+                    lr = learning_rate, N_updates_W = N_updates_W,N_updates_C = N_updates_C ,num_epochs = num_epochs, batch_size = batch_size, num_layers=num_layers, gamma_c=gamma_c, gamma_w=gamma_w, eps=eps, nu=nu, zeta=zeta):
+            """
+            Parameters
+            ----------
+                test_conditions    (list): list of 5 elements, the name of the blur kernel (str), the noise level (str), the range of the noise standard deviation (list), 
+                                        the image size (numpy array), minimal and maximal pixel values (list)
+                folders            (list): list of str, paths to the folder containing (i) the test sets, (ii) the training, (iii) saved models
+                mode                (str): 'first_layer' if training the first layer, 'greedy' if training the following layers one by one,
+                                        'last_layers_lpp' if training the last 10 layers + lpp, 'test' if testing the model (default is 'first_layer')
+                lr_first_layer     (list): list of two elements, first one is the initial learning rate to train the first layer, 
+                                        second one is the number of epochs after which the learning rate is multiplied by 0.9 (default is [1e-2,5])    
+                lr_greedy          (list): list of two elements, first one is the initial learning rate to train the layers, 
+                                        second one is the number of epochs after which the learning rate is multiplied by 0.9 (default is [5e-3,5])
+                lr_last_layers_lpp (list): list of two elements, first one is the initial learning rate to train the last 10 layers conjointly with lpp, 
+                                        second one is the number of epochs after which the learning rate is multiplied by 0.9 (default is [1e-3,50])  
+                nb_epochs          (list): list of three integers, number of epochs for training the first layer, the remaining layers, 
+                                        and the last 10 layers + lpp, respectively (default is [40,40,600])      
+                nb_blocks           (int): number of unfolded iterations (default is 40)    
+                nb_greedy_blocks    (int): number of blocks trained in a greedy way (default is 30)
+                batch_size         (list): list of three integers, number of images per batch for training, validation and testing, respectively (default is [10,10,1])                
+                loss_type           (str): name of the training loss (default is 'SSIM')  
+            """
+            super(U_TITAN, self).__init__()           
+            # unpack information about test conditions and saving folders
+            self.T = T
+            self.K = K
+            self.N = N
+            self.input_dim = input_dim
+            self.metaparameters_multiparam = metaparameters_multiparam
+            self.dataset_size = size
+            #self.path_test, self.path_train, self.path_save = folders
+            self.mode  = mode #'first_layer' or 'greedy' or 'last_layers_lpp' or 'test'
+            # training information
+            self.lr = lr
+            self.num_epochs = num_epochs 
+            self.num_layers = num_layers
+            self.batch_size = batch_size 
+            self.dtype = torch.cuda.FloatTensor
+            self.model = myModel(input_dim, N_updates_W, N_updates_C, num_layers=num_layers, gamma_c=gamma_c, gamma_w=gamma_w, eps=eps, nu=nu, zeta=zeta).cuda()
+            self.loss_fun = ISI_loss()
 
 
-optimizer = optim.Adam(alpha_net_params, lr=0.001)
-# Training loop
-for epoch in range(num_epochs):
-    for X, A in dataloader:
-        X = X[0]
-        A = A[0]
+
+    def CreateLoader(self, dataset, batch_size):
+        """
+        Create a DataLoader object from a dataset
+
+        Parameters
+        ----------
+            dataset (torch.utils.data.Dataset): dataset to be loaded
+            batch_size (int): number of images per batch
+
+        Returns
+        -------
+            DataLoader object
+        """
+
+        train_data = MyDataset(self.T, self.K, self.N, self.metaparameters_multiparam, size = self.dataset_size)
+        #val_data   = MyDataset(self.T, self.K, self.N, self.metaparameters_multiparam, size = self.dataset_size)
+        self.size_train = train_data.size
+        self.train_loader = DataLoader(train_data, batch_size=self.batch_size, shuffle=True)
+        #self.val_loader   = DataLoader(val_data, batch_size=self.batch_size, shuffle=True)
+
+
+    def CreateFolders(self,layer):
         
-        #print(f"Batch X shape: {X.shape}, Batch A shape: {A.shape}")
+        """
+        
+            
+            Create folders to save the results of the training and testing
 
-        # Forward pass to compute ISI scores and update parameters
-        model.train()
-        optimizer.zero_grad()
-        W, C, _ = model(X, A)
-        isi_score = joint_isi(W, A)
-        print(f"ISI score before backward: {isi_score.item()}")
-        isi_score.backward()
-        optimizer.step()
-    
-    print(f"Epoch {epoch + 1}/{num_epochs}, ISI Score: {isi_score.item()}")
-    """     
-    for name, param in model.named_parameters():
-        if param.requires_grad:
-            print(name, param.data) """
+            
+
+            if self.mode=='first_layer' or self.mode=='greedy':
+                name = 'block_'+str(layer)
+                if not os.path.exists(os.path.join(self.path_save,name)):
+                    os.makedirs(os.path.join(self.path_save,name,'training'))
 
 
-# After training, you can access the trained model parameters, including the optimized alpha values, using model.parameters().
+            # create the folder to save the results of the training
+            if not os.path.exists(self.path_save):
+                os.makedirs(self.path_save)
+            # create the folder to save the results of the testing
+            if not os.path.exists(self.path_test):
+                os.makedirs(self.path_test)
+            # create the folder to save the results of the testing
+            if not os.path.exists(self.path_test):
+                os.makedirs(self.path_test)
+        
+        """
 
 
+    def train(self,layer=0):
+        """
+        Trains U_TITAN.
+        Parameters
+        ----------
+            layer (int): number of the layer to be trained, numbering starts at 0 (default is 0)
+        """   
+        if self.mode == 'first_layer':
+            #self.train_first_layer()
+            pass
 
+        elif self.mode == 'greedy':
+            # trains the next layer
+            print('=================== Layer number %d ==================='%(layer))
+            # to store results
+            loss_epochs       =  np.zeros(self.num_epochs)
+            isi_train   =  np.zeros(self.num_epochs)
+            isi_val     =  np.zeros(self.num_epochs)
+            loss_min_val      =  float('Inf')
+            self.CreateFolders(layer)
+            #folder = os.path.join(self.path_save,'Layer_'+str(layer))
+            dataset = MyDataset(T, K, N, metaparameters_multiparam, size = dataset_size)
+            self.CreateLoader(dataset=dataset, batch_size=self.batch_size)
+            # puts first blocks in evaluation mode: gradient is not computed
+            self.model.GradFalse(layer,self.mode) 
+            # defines the optimizer
+            lr = self.lr
+            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad,self.parameters()),lr=lr)
+            #==========================================================================================================
+            # trains for several epochs
+            for epoch in range(0,self.num_epochs):
+                
+                self.model.Layers[layer].train() # training mode
+                # goes through all minibatches
+                for i,minibatch in enumerate(self.train_loader,0):
+                    [X, A] = minibatch  # gets the minibatch
+                    #print("minibatch size: ", minibatch[0].size())
+                    X = X[0]
+                    A = A[0]
+                    Rx = cov_X(X)
+                    W,C = initialize(N,K,X=X,Rx=Rx)
 
+                    W_predicted,C_predicted = self.model(Rx,W,C,self.mode,layer)
 
+                    # Computes and prints loss
+                    loss = self.loss_fun(W_predicted, A)
+                    loss_epochs[epoch] += torch.Tensor.item(loss)
+                    sys.stdout.write('\r Epoch %d/%d, minibatch %d/%d, loss: %.4f' % (epoch+1,self.num_epochs,i+1,self.size_train//self.batch_size,loss))
+                    
+                    # sets the gradients to zero, performs a backward pass, and updates the weights.
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                # tests on validation set
+                
+            # training is finished
+            print('-----------------------------------------------------------------')
+            print('Training of Layer ' + str(layer) + ' is done.')
+            print('-----------------------------------------------------------------')
+            
+            # calls the same function to start training of next block 
+            if layer < self.num_layers-1:
+                self.train(layer=layer+1)
+            else:
+                print('Training of all layers is done.')
+        
+        ###############################################################################################################
+        
+
+        
+test = U_TITAN()
+test.train()
--- TITAN_Unrolled/model.py
+++ TITAN_Unrolled/model.py
@@ -1,3 +1,20 @@
+"""
+UTitan model classes
+Classes
+-------
+    ISI_loss  : defines the ISI training loss 
+    nn_alpha    : predicts the regularisation parameter
+    W_iter     : computes the updates of W
+    C_iter    : computes the updates of C
+    Block      : one layer in U_TITAN
+    myModel    : U_TITAN model
+
+
+@author: Gaspard Blaise
+@date: 11/06/2024
+"""
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,22 +23,302 @@ from functions import *
 from tools import *
 
 
+class ISI_loss():
+    """
+    Defines the ISI training loss.
+    Attributes
+    ----------
+        ISI : function computing the ISI Score 
+    """
+    def __init__(self): 
+        super(ISI_loss, self).__init__()
+        
+ 
+    def __call__(self, input, target):
+        """
+        Computes the training loss.
+        Parameters
+        ----------
+            input  (torch.FloatTensor): restored images, size n*c*h*w 
+            target (torch.FloatTensor): ground-truth images, size n*c*h*w
+        Returns
+        -------
+            (torch.FloatTensor): ISI Score, size 1 
+        """
+        return joint_isi(input, target)
 
-class AlphaNetwork(nn.Module):
-    def __init__(self, input_dim):
-        super(AlphaNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 1,device='cuda:0')
-        #print(input_dim)
-        self.activation = nn.Softmax()
 
+
+class FCNN_alpha(nn.Module):
+    """
+    Predicts the regularization parameter alpha given W and C.
+    Attributes
+    ----------
+        fc1, fc2, fc3 (torch.nn.Linear): fully connected layers
+        soft          (torch.nn.Softplus): Softplus activation function
+    """
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size=1):
+        super(FCNN_alpha, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.fc3 = nn.Linear(hidden_size2, output_size)
+        self.soft = nn.Softplus()
+        
     def forward(self, W, C):
-        x = torch.cat((W.flatten(), C.flatten()), dim=0)
-        #print('W', W.device)
-        #print('C', C.device)
-        #print("x", x.device)
-        alpha = self.activation(self.fc1(x))
-        return alpha
+        """
+        Computes the regularization parameter alpha.
+        Parameters
+        ----------
+            W (torch.FloatTensor): input tensor W, size [batch_size, W_size]
+            C (torch.FloatTensor): input tensor C, size [batch_size, C_size]
+        Returns
+        -------
+            torch.FloatTensor: alpha parameter, size [batch_size, 1]
+        """
+        # Flatten W to size [N*N*K]
+        W = W.view(-1)
+        # Flatten C to size [K*K*N]
+        C = C.view(-1)
+        # Concatenate W and C along the last dimension
+        x = torch.cat((W, C))
+        x = self.soft(self.fc1(x))
+        x = self.soft(self.fc2(x))
+        x = self.fc3(x)
+        x = self.soft(x)
+        return x
+
+
+
+
+
+
+class W_iter(nn.Module):
+
+    def __init__(self,N_updates_W):
+        super(W_iter, self).__init__()
+        self.N_updates_W = N_updates_W
+
+    def inertial_step(self,beta_w,W,W_old):
+        W = W + beta_w * (W - W_old)
+        return W
     
+    def gradient_step(self,Rx,c_w,W,C):
+        W = W - c_w * grad_H_W(W, C, Rx)
+        return W
+
+    def prox_step(self,c_w,W):
+        return prox_f(W, c_w)
+
+    def update(self,Rx,W,W_old,C,c_w,beta_w):
+        W_inertial = self.inertial_step(beta_w,W,W_old)
+        W_old = W.clone()
+        W_gradient = self.gradient_step(Rx,c_w,W_inertial,C)
+        W_prox = self.prox_step(c_w,W_gradient)
+        return W_prox,W_old
+    
+    def forward(self,Rx,W,C,c_w,beta_w):
+        W_old = W.clone()
+        for _ in range(self.N_updates_W):
+            W,W_old = self.update(Rx,W,W_old,C,c_w,beta_w)
+        return W
+    
+
+
+class C_iter(nn.Module):
+    def __init__(self,N_updates_C):
+        super(C_iter, self).__init__()
+        self.N_updates_C = N_updates_C
+
+    def inertial_step(self,beta_c,C,C_old):
+        C = C + beta_c * (C - C_old)
+        return C
+    
+    def gradient_step(self,Rx,c_c,C,W,alpha):
+        C = C - c_c * grad_H_C_reg(W, C, Rx, alpha)
+        return C
+
+    def prox_step(self,c_c,C,eps):
+        return prox_g(C, c_c, eps)
+    
+    def update(self,Rx,C,C_old,W_updated,c_c,beta_c,alpha,eps):
+        C_inertial = self.inertial_step(beta_c,C,C_old)
+        C_old = C.clone()
+        C_gradient = self.gradient_step(Rx,c_c,C_inertial,W_updated,alpha)
+        C_prox = self.prox_step(c_c,C_gradient,eps)
+        return C_prox,C_old
+
+    def forward(self,Rx,C,W_updated,c_c,beta_c,alpha,eps):
+        for _ in range(self.N_updates_C):
+            C_old = C.clone()
+            C,C_old = self.update(Rx,C,C_old,W_updated,c_c,beta_c,alpha,eps)
+            return C,C_old
+
+
+
+
+class Block(nn.Module):
+
+    """
+    One layer in U_TITAN.
+    Attributes
+    ----------
+        nn_bar                           (Cnn_bar): computes the barrier parameter
+        soft                    (torch.nn.Softplus): Softplus activation function
+        gamma                (torch.nn.FloatTensor): stepsize, size 1 
+        reg_mul,reg_constant (torch.nn.FloatTensor): parameters for estimating the regularization parameter, size 1
+        delta                               (float): total variation smoothing parameter
+        IPIter                             (IPIter): computes the next proximal interior point iterate
+    """
+
+
+    def __init__(self,input_dim, N_updates_W,N_updates_C,gamma_c,gamma_w,eps,nu,zeta):
+    
+        super(Block, self).__init__()
+        self.input_dim = input_dim
+        self.NN_alpha = FCNN_alpha(input_dim, 64, 32)
+        #self.soft         = nn.Softplus()
+        #self.reg_mul      = nn.Parameter(torch.FloatTensor([-7]).cuda()) 
+        #self.reg_constant = nn.Parameter(torch.FloatTensor([-5]).cuda())
+        self.W_iter = W_iter(N_updates_W)
+        self.C_iter = C_iter(N_updates_C)
+        self.gamma_c = gamma_c
+        self.gamma_w = gamma_w
+        self.eps = eps
+        self.nu = nu
+        self.zeta = zeta
+    
+    def get_coefficients(self,Rx,alpha,C,C_old):
+        
+        K,_,N = C.shape
+        #print("alpha",alpha)
+
+        rho_Rx = spectral_norm_extracted(Rx,K,N)
+        #print("rho_Rx",rho_Rx)
+        l_sup = torch.max((self.gamma_w*alpha)/(1-self.gamma_w),rho_Rx*2*K*(1+torch.sqrt(2/(alpha*self.gamma_c))))
+
+        expr3 = torch.tensor(self.gamma_c**2 / K**2, dtype=torch.float32)
+        expr4 = alpha * self.gamma_w / ((1 + self.zeta) * (1 - self.gamma_w) * l_sup)
+        expr5 = rho_Rx / ((1 + self.zeta) * l_sup)
+
+        # Compute the minimum of the three expressions
+        C0 = torch.min(torch.min(expr3, expr4), expr5)
+
+
+        L_inf = (1+self.zeta)*C0*l_sup
+        L_w_prev = torch.max(L_inf,lipschitz(C_old,rho_Rx))
+        L_w = torch.max(L_inf,lipschitz(C,rho_Rx))
+
+        c_w = self.gamma_w/L_w
+        beta_w = (1-self.gamma_w)*torch.sqrt(C0*self.nu*(1-self.nu)*L_w_prev/L_w)
+
+        c_c = self.gamma_c/alpha
+        beta_c = torch.sqrt(C0 * self.nu*(1-self.nu))
+
+        return c_w,beta_w,c_c,beta_c
+
+    def forward(self,Rx,W,C,C_old):
+        """
+        Computes the next iterate, output of the layer.
+        Parameters
+        ----------
+      	    x            (torch.nn.FloatTensor): previous iterate, size n*c*h*w
+            Ht_x_blurred (torch.nn.FloatTensor): Ht*degraded image, size n*c*h*w
+            std_approx   (torch.nn.FloatTensor): approximate noise standard deviation, size n*1
+            save_gamma_mu_lambda          (str): indicates if the user wants to save the values of the estimated hyperparameters, 
+                                                 path to the folder to save the hyperparameters values or 'no' 
+        Returns
+        -------
+       	    (torch.FloatTensor): next iterate, output of the layer, n*c*h*w
+        """
+
+
+        alpha = self.NN_alpha(W,C)
+        c_w,beta_w,c_c,beta_c = self.get_coefficients(Rx,alpha,C,C_old)
+
+        W_updated = self.W_iter(Rx,W,C,c_w,beta_w)
+        C_updated,C_old = self.C_iter(Rx,C,W_updated,c_c,beta_c,alpha,self.eps)
+        return W_updated,C_updated,C_old
+
+
+
+
+class myModel(nn.Module):
+    """
+    U_TITAN model.
+    Attributes
+    ----------
+        blocks (list): list of Blocks
+    """
+    def __init__(self,input_dim, N_updates_W,N_updates_C,num_layers,gamma_c,gamma_w,eps,nu,zeta):
+        super(myModel, self).__init__()
+        self.Layers = nn.ModuleList([Block(input_dim,N_updates_W,N_updates_C,gamma_c,gamma_w,eps,nu,zeta) for _ in range(num_layers)])
+
+
+
+    def GradFalse(self,block,mode):
+        """
+        Initializes current layer's parameters with previous layer's parameters, fixes the parameters of the previous layers.
+        Parameters
+        ----------
+      	    block (int): block-1 is the layer to be trained
+            mode  (str): 'greedy' if training one layer at a time, 'last_layers_lpp' if training the last 10 layers + lpp
+        """
+        if block>=1:
+            if mode=='greedy':
+                self.Layers[block].load_state_dict(self.Layers[block-1].state_dict())
+            for i in range(0,block):
+                self.Layers[i].eval()
+                for p in self.Layers[i].parameters():
+                    p.requires_grad = False
+                    p.grad          = None
+
+
+    def forward(self,Rx,W,C,mode,layer=0):
+        """
+        Computes the next iterate, output of the model.
+        Parameters
+        ----------
+      	    W,C            (torch.nn.FloatTensor): previous iterates, size n*N*N*K, n*K*K*N
+            Rx             (torch.nn.FloatTensor): covariance matrix of the input, size K*K*N
+            mode          (str): 'first_layer' if training the first layer, 'greedy' if training one layer at a time, 'test' if testing
+            layer                         (int): layer-1 is the layer to be trained (default is 0)
+            save_gamma_mu_lambda          (str): indicates if the user wants to save the values of the estimated hyperparameters, 
+                                                 path to the folder to save the hyperparameters values or 'no' 
+        Returns
+        -------
+       	    (torch.FloatTensor): next iterates, output of the model, n*N*N*K, n*K*K*N
+        """
+        if mode=='first_layer' or mode=='greedy':
+            C_old = C.clone()
+            W,C,C_old = self.Layers[layer](Rx,W,C,C_old)
+        elif mode=='test':
+            for i in range(len(self.Layers)):
+                # we use .detach() to avoid computing and storing the gradients since the model is being tested
+                C_old = C.clone()
+                W,C,C_old = self.Layers[i](Rx,W,C,C_old)
+
+        return W,C
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
 
 
 class DeterministicBlock(nn.Module):
@@ -130,11 +427,11 @@ class TitanIVAGNet(nn.Module):
 
 
         for i, layer in enumerate(self.layers):
-            layer.alpha_net = AlphaNetwork(input_dim)  # Ensure each layer has its own alpha_net
+
             layer.deterministic_block = DeterministicBlock(Rx, rho_Rx, self.gamma_c, self.gamma_w, self.eps, self.nu, self.zeta)  # Ensure each layer has its own deterministic block
             W, C, L_w, alpha = layer(W, C, L_w_prev)
             L_w_prev = L_w
-            self.alphas[i] = alpha.item()
+            self.alphas[i] = alpha
             
 
         
@@ -142,3 +439,4 @@ class TitanIVAGNet(nn.Module):
 
         return W, C, isi_score
 
+"""
