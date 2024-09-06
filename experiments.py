49e3b84b1303cfe05cf7b59278136e32ea04c532
Clément <clement.cosserat@gmail.com>
2024-09-06 11:30:03 +0200

    new results to validate the choice alpha = 1

--- experiments.py
+++ experiments.py
@@ -13,15 +13,15 @@ lambda_1 = 0.04
 lambda_2 = 0.25
 rho_bounds_1 = [0.2,0.3]
 rho_bounds_2 = [0.6,0.7]
-rhos = [rho_bounds_1] #,rho_bounds_2]
-lambdas = [lambda_1] #,lambda_2]
+rhos = [rho_bounds_1,rho_bounds_2]
+lambdas = [lambda_1,lambda_2]
 # identifiability_levels = [1e-2,1e-1,1])
 # identifiability_levels_names = ['low identifiability','medium identifiability','high identifiability']
 Ks = [10,20,50]
 Ns = [10,20] #,10]
 
 common_parameters = [Ks,Ns]
-metaparameters_titles_multiparam = ['Case A'] #,'Case B','Case C','Case D']
+metaparameters_titles_multiparam = ['Case A','Case B','Case C','Case D']
 
 def get_metaparameters(rhos,lambdas):
     metaparameters_multiparam = []
@@ -41,11 +41,9 @@ plt.rcParams['text.usetex'] = True
 metaparameters_multiparam = get_metaparameters(rhos,lambdas)
 
 
-algo_titan1 = TitanIvaG((0,0.4,0),name='titan1',alpha=0.1,gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='numpy')
-algo_titan2 = TitanIvaG((0,0.4,0),name='titan2',alpha=0.5,gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='numpy')
-algo_titan3 = TitanIvaG((0,0.4,0),name='titan3',alpha=1,gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='numpy')
-algo_titan4 = TitanIvaG((0,0.4,0),name='titan4',alpha=5,gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='numpy')
-algo_titan5 = TitanIvaG((0,0.4,0),name='titan5',alpha=10,gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='numpy')
+
+algo_titan = TitanIvaG((0,0.4,0),name='titan',alpha=1,gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='numpy')
+algo_palm = TitanIvaG((0,0.4,0),name='palm',alpha=1,gamma_w=0.99,gamma_c = 1.99,nu=0,crit_ext=1e-10,crit_int=1e-10,library='numpy')
 # algo_titan_torch = TitanIvaG((0,0.8,0),name='titan_torch',gamma_w=0.99,crit_ext=1e-10,crit_int=1e-10,library='torch')
 # algo_iva_g_n_numpy = IvaG((0.5,0,0),name='iva_g_n_numpy',legend='NN',crit_ext=1e-7,opt_approach='newton',library='numpy')
 # algo_iva_g_n_torch = IvaG((1,0,0),name='iva_g_n_torch',legend='NT',crit_ext=1e-7,opt_approach='newton',library='torch')
@@ -57,7 +55,7 @@ algo_titan5 = TitanIvaG((0,0.4,0),name='titan5',alpha=10,gamma_w=0.99,crit_ext=1
 # algo_fast_iva_g_n_numpy = IvaG((0.5,0,0.5),name='fast_iva_g_n_numpy',legend='FNN',crit_ext=1e-7,opt_approach='newton',library='numpy',fast=True)
 
 
-algos = [algo_titan1,algo_titan2,algo_titan3,algo_titan4,algo_titan5] #algo_fast_iva_g_v_torch,algo_fast_iva_g_n_numpy,algo_fast_iva_g_n_torch] #,algo_iva_g_v_numpy,algo_iva_g_v_torch,algo_iva_g_n_torch,algo_iva_g_n_numpy] #,algo_iva_g_v_torch] #,algo_titan_numpy,algo_titan_torch,algo_iva_g_v_numpy,algo_iva_g_n_numpy]
+algos = [algo_titan,algo_palm] #algo_fast_iva_g_v_torch,algo_fast_iva_g_n_numpy,algo_fast_iva_g_n_torch] #,algo_iva_g_v_numpy,algo_iva_g_v_torch,algo_iva_g_n_torch,algo_iva_g_n_numpy] #,algo_iva_g_v_torch] #,algo_titan_numpy,algo_titan_torch,algo_iva_g_v_numpy,algo_iva_g_n_numpy]
 
 exp2 = ComparisonExperimentIvaG('alpha benchmark',algos,metaparameters_multiparam,metaparameters_titles_multiparam,
                                 common_parameters,'multiparam',title_fontsize=50,legend_fontsize=6,N_exp=50,charts=False,legend=False)
